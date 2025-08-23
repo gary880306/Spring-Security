d83128aaa54091dba9f4b1fe05f492889225965b
gary.chen <ch66971542@gmail.com>
2025-08-23 16:44:32 +0800

    feat: 添加SpringSecurity

---
+++ src/main/java/com/gary/springsecurity/controller/MyController.java
@@ -0,0 +1,19 @@
+package com.gary.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome!";
+    }
+
+}
