87ad9a8d3620209ea2ff06b718a3a597ef375222
gary.chen <ch66971542@gmail.com>
2025-10-18 16:32:40 +0800

    fix: 註冊帳號-密碼改為Hash加密
    - BCrypt

--- src/main/java/com/gary/springsecurity/controller/MemberController.java
+++ src/main/java/com/gary/springsecurity/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.gary.springsecurity.controller;
 import com.gary.springsecurity.dao.MemberDao;
 import com.gary.springsecurity.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +14,15 @@ public class MemberController {
     @Autowired
     private MemberDao memberDao;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public Member register(@RequestBody Member member) {
         // 省略參數檢查
+
+        // 原始密碼hash
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
 
         // db插入一筆新的 Member 數據
         Integer memberId = memberDao.createMember(member);
