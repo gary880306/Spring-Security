80a780618ecb8242f7352fe4b6e0b3e31404f835
gary.chen <ch66971542@gmail.com>
2025-10-20 20:25:13 +0800

    feat: 自定義Filter
    - 在自定義Filter中處理request & response

--- src/main/java/com/gary/springsecurity/config/MyFilter2.java
+++ src/main/java/com/gary/springsecurity/config/MyFilter2.java
@@ -13,7 +13,15 @@ public class MyFilter2 extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("執行 MyFilter2");
 
-        // 傳遞給下一個Filter，繼續處理
-        filterChain.doFilter(request, response);
+        if (request.getRequestURI().contains("/hello")) {
+            System.out.println("允許通過");
+            // 傳遞給下一個Filter，繼續處理
+            filterChain.doFilter(request, response);
+        } else {
+            System.out.println("不允許通過，中斷請求");
+            response.setStatus(500);
+        }
+
+
     }
 }
