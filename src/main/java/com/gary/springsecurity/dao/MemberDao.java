1a3a7fb0724e9b185c6d1957485f428c8d2a9218
gary.chen <ch66971542@gmail.com>
2025-10-26 17:51:11 +0800

    feat: 單體架構實戰演練-串接資料庫
    - myMovie

---
+++ src/main/java/com/gary/springsecurity/dao/MemberDao.java
@@ -0,0 +1,20 @@
+package com.gary.springsecurity.dao;
+
+import com.gary.springsecurity.model.Member;
+import com.gary.springsecurity.model.Role;
+
+import java.util.List;
+
+public interface MemberDao {
+
+    Member getMemberById(Integer memberId);
+
+    Member getMemberByEmail(String email);
+
+    Integer createMember(Member member);
+
+    List<Role> getRolesByMemberId(Integer memberId);
+
+    String getName();
+
+}
