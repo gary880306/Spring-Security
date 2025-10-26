61501459f8c576937cce7290386cec8ed2a86f3c
gary.chen <ch66971542@gmail.com>
2025-10-26 17:54:22 +0800

    feat: 單體架構實戰演練-實作登入API
    可做可不做，SpringSecurity基本上已經完成登入

--- src/main/java/com/gary/springsecurity/config/MySecurityConfig.java
+++ src/main/java/com/gary/springsecurity/config/MySecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,11 +22,14 @@ public class MySecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                // 設定 Session 創建機制
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
                 .csrf(csrf -> csrf.disable())
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .authorizeHttpRequests(request -> request
                         .requestMatchers("/register").permitAll() // 註冊帳號
+                        .requestMatchers("/userLogin").authenticated() // 登入功能
                         .anyRequest().authenticated()
                 )
                 .build();
--- src/main/java/com/gary/springsecurity/controller/MemberController.java
+++ src/main/java/com/gary/springsecurity/controller/MemberController.java
@@ -3,10 +3,14 @@ package com.gary.springsecurity.controller;
 import com.gary.springsecurity.dao.MemberDao;
 import com.gary.springsecurity.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class MemberController {
@@ -27,6 +31,15 @@ public class MemberController {
         // db插入一筆新的 Member 數據
         Integer memberId = memberDao.createMember(member);
         return "註冊成功";
+    }
+
+    @PostMapping("/userLogin")
+    public String userLogin(Authentication authentication) {
+        // 取得使用者帳號
+        String userName = authentication.getName();
+        // 取得始御者權限
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        return "登入成功！帳號： " + userName + " 的權限為： " + authorities;
     }
 
 }
